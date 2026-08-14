89918752b4af0079dc4e97a64b49cc3753e5f048
cogito30 <cogito30g@gmail.com>
2026-08-14 12:56:16 +0900

    modify: GPT 재구성하기

--- tiny_gpt2/README.md
+++ tiny_gpt2/README.md
@@ -1,29 +1,45 @@
 # Project: GPT-2 구현하기
 
+- Reference: [GPT-2](https://cdn.openai.com/better-language-models/language_models_are_unsupervised_multitask_learners.pdf)
+
+
 ## 1단계. 텍스트 데이터 처리
 - Tokenizer 구현하기
 1) 텍스트 읽어오기
 2) 텍스트 전처리: 특수토큰, 토큰으로 나누기
+- 특수 토큰 처리: `<unk>`, `<endoftext>`, `<bos>`, `<eos>`, `<pad>`
 3) 어휘사전 생성하기
 4) 토큰화된 텍스트를 토큰 ID로 변환하기
+5) BPE 사용하기
 
-- Dataset && DataLoader 구현하기
-  - input && target 쌍으로 나누기
-  - sliding window 방식으로 구현하기
-  - 배치 사이에 중첩이 있으면 overfitting 증가
+## 2단계. Dataset && DataLoader 구현하기
+- input && target 쌍으로 나누기
+- sliding window 방식으로 구현하기
+- 배치 사이에 중첩이 있으면 overfitting 증가
 
-- 단어 임베딩하기
-  - 임베딩은 LUT로 동작함을 확인하기
-
+## 3단계. 단어 임베딩하기
+- 임베딩은 LUT로 동작함을 확인하기
 - position embedding 인코딩하기
   - 절대 위치 임베딩 사용하기
 
-## 2단계. Attention 계층 구현
+## 4단계. Attention 계층 구현
 - self-attention 구현하기
 1) 입력 임베딩 벡터에서 query 선택
 2) query와 입력 임베딩 벡터를 dot product해서 attention_score 구하기
-3) attention_score를 배치 정규화해서 attention_weight로 만들기
+3) attention_score를 층 정규화해서 attention_weight로 만들기
 4) attention_wieght에 각 임베딩 벡터(key)를 곱하고 더해서 context_vector 만들기
 
 - 정규화시 attention_score를 key의 임베딩 차원의 제곱근으로 나누기
 - multi-head attention 구현하기
+- causal mask 처리
+
+## 5단계 GPT 구현하기
+1) 전체 구조 잡기
+2) 각 Layer 구현하기
+- LayerNorm 구현
+- GELU, SwiGLU 활성화 함수 구현
+- FeedForward 모듈 구현
+- Transformer Block 구현하기
+  - Shortcut Connection 구현
+3) GPT-2 모델 구현
+- 토큰 임베딩 층과 출력 층의 가중치 묶기
--- tiny_gpt2/layer.py
+++ tiny_gpt2/layer.py
@@ -49,7 +49,75 @@ class MultiHeadAttention(nn.Module):
 
         return context_vector
 
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim, eps=1e-5):
+        super().__init__()
+        self.eps = eps
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, correction=0)    # correction: Bessel's correction 적용 여부
+        norm_x = (x- mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+
+
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(torch.sqrt(torch.tensor(2.0 / torch.pi)) * (x + 0.044715 * torch.pow(x, 3))))
+
+
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
+            GELU(),
+            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"])
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.attentions = MultiHeadAttention(
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            context_length=cfg["context_length"],
+            num_heads=cfg["n_heads"],
+            dropout=cfg["drop_rate"],
+            qkv_bias=cfg["qkv_bias"]
+        )
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg["emb_dim"])
+        self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
+
+    def forward(self, x):
+        shortcut = x
+        x = self.norm1(x)
+        x = self.attentions(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+        return x
+
+
+
 if __name__ == "__main__":
+    import cfg
     torch.manual_seed(21)
     inputs = torch.tensor(
         [[0.43, 0.15, 0.89],
@@ -67,3 +135,39 @@ if __name__ == "__main__":
     context_vecs = mha(batch)
     print(context_vecs)
     print(f"shape: {context_vecs.shape}")
+
+
+    print("---LayerNorm---")
+    torch.manual_seed(21)
+    batch_example = torch.rand(2, 5)
+    print(f"bacth: {batch_example}")
+
+    ln = LayerNorm(emb_dim=5)
+    out_ln = ln(batch_example)
+    print(out_ln)
+    mean = out_ln.mean(dim=-1, keepdim=True)
+    var = out_ln.var(dim=-1, keepdim=True)
+    print(f"mean: {mean}")
+    print(f"var: {var}")
+
+    import matplotlib.pyplot as plt
+    gelu, relu = GELU(), nn.ReLU()
+
+    x = torch.linspace(-3, 3, 100)
+    y_gelu, y_relu = gelu(x), relu(x)
+    plt.figure(figsize=(8, 3))
+    for i, (y, label) in enumerate(zip([y_gelu, y_relu], ["GELU", "ReLU"]), 1):
+        plt.subplot(1, 2, i)
+        plt.plot(x, y)
+        plt.title(f"{label} activation function")
+        plt.xlabel("x")
+        plt.ylabel(f"{label}(x)")
+        plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+    print("---Test FeedForward---")
+    ffn = FeedForward(cfg.GPT_CONFIG_124M)
+    x = torch.rand(2, 3, 768)
+    out = ffn(x)
+    print(out.shape)
